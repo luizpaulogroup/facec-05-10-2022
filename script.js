367bde2bca5646d4eb46830b8441fad84244e219
LP <luizpaulogroup@gmail.com>
2022-09-26 20:42:53 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Application started");
+
+let form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+
+    let name = form.name.value;
+    let email = form.email.value;
+    let notification = form.notification.checked;
+
+    console.log(name);
+    console.log(email);
+    console.log(notification);
+});
